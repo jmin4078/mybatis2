fe4fec6ba22df12229b3dbfb2aada19cafca1ada
jmin4078 <wpalsrl8588@gmail.com>
2026-07-22 10:12:06 +0900

    Initial commit

---
+++ src/main/java/org/example/mybatis2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.example.mybatis2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Mybatis2Application.class);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 26. 7. 22.
+  Time: 오전 10:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello MyBatis!</title>
+</head>
+<body>
+<h1>Hello MyBatis!</h1>
+</body>
+</html>
